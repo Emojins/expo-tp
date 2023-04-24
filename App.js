ef81e9993dab3c90f2c642ace841570bc47f1df6
Nahuel Paniagua <nahu.1999@outlook.com>
2023-04-24 12:43:39 -0300

    trabajo de boton desplegable

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View, Pressable } from 'react-native'
 
 export default function App() {
+  const [Mostrar, modMostrar] = useState(false);
+  const vistasDos = [
+      { compa: 'Dieguito', apellido: 'Brito' },
+      { compa: 'Lucia', apellido: 'Alla' },
+      { compa: 'Lucas', apellido: 'Duran' },
+      { compa: 'Tamara', apellido: 'Queso' },
+      { compa: 'Kiko', apellido: 'Dreven' },
+  ]
+  
+  
+
+  const mostrargilada = ()=>{
+    modMostrar(!Mostrar);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: '25px'}} >Compañeros del curso: </Text>
+      <Pressable style={({ pressed }) => ({ backgroundColor: pressed ? 'blue': 'blue' })}
+        onPress={mostrargilada}>
+        <Text style={styles.botonKP}>Presiona para ver los compañeros del curso kp</Text>
+      </Pressable>
+      {Mostrar && (
+        <View>
+          <FlatList data={vistasDos} renderItem={({ item }) => <Text>{` nombre: ${item.compa}    apellido: ${item.apellido}`}</Text>}/>
+        </View>
+      )}
+       <StatusBar style="auto" />
     </View>
-  );
+     
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(140,234,213)',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  texto: {
+    fontFamily: 'Algerian',
+    fontSize: '40px'
+  },
+  botonKP:{
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: 'blue'
+  }
 });
